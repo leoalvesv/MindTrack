7300f7eb08813d618578593d42eccd2140c11eba
leoalvesv <leoalvesvianna@gmail.com>
2025-10-18 20:19:09 -0300

    Initial Commit

---
+++ MindTrack/MindTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindTrackApp.swift
+//  MindTrack
+//
+//  Created by Leonardo Alves Viana on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MindTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
